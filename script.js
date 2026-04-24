848a5605eeaa98bce97f47b8186d3d9a99f8920c
faizankhan <faijankha7860@gmail.com>
2026-04-24 17:07:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,8 +97,18 @@ text.innerText = "🤖 Loading AI Models...";
 
 try {
 
-await faceapi.nets.tinyFaceDetector.loadFromUri("models");
-await faceapi.nets.faceExpressionNet.loadFromUri("models");
+ const MODEL_URL =
+"https://faizankhan07867.github.io/Mood-Based-Learning-Website_/models";
+
+await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+
+const stream =
+await navigator.mediaDevices.getUserMedia({
+video:true
+});
+
+
 camera.srcObject = stream;
 
 camera.onloadedmetadata = function(){
